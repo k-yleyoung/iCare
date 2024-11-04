96162486c5b9bb536960cb78bf405efcfc19143f
mcurry572 <130413075+mcurry572@users.noreply.github.com>
2024-11-04 16:28:53 -0600

    view patient button in icare as well as treatment/documents

--- iCare/Models/Document.cs
+++ iCare/Models/Document.cs
@@ -6,5 +6,6 @@
         public required string FileName { get; set; }
         public required byte[] FileContent { get; set; }
         public required string ContentType { get; set; }
+        public int PatientId { get; internal set; }
     }
 }
